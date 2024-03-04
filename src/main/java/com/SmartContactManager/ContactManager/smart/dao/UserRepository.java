390d8ce4e64f8035f0cdc01b834ca13151397799
rahulRathod1234 <rahulrathod34615@gmail.com>
2024-03-04 22:35:58 +0530

    custom login form and user dashboard added

--- src/main/java/com/SmartContactManager/ContactManager/smart/dao/UserRepository.java
+++ src/main/java/com/SmartContactManager/ContactManager/smart/dao/UserRepository.java
@@ -3,12 +3,10 @@ package com.SmartContactManager.ContactManager.smart.dao;
 import com.SmartContactManager.ContactManager.Entities.User;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 
 public interface UserRepository  extends JpaRepository<User,Integer>
 {
 
-    @Query("select u from user u where u.email=:email")
-    public  User getUserByUserName(@Param("email") String email);
+
 }
--- src/main/java/com/SmartContactManager/ContactManager/smartConfiguration/MyConfig.java
+++ src/main/java/com/SmartContactManager/ContactManager/smartConfiguration/MyConfig.java
@@ -1,10 +1,14 @@
 package com.SmartContactManager.ContactManager.smartConfiguration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class MyConfig
-{
-
+public class MyConfig {
 
 }
+
+
+
+
