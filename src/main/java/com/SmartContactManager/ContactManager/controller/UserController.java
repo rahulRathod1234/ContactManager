b8756e232251bc2350e40d669004f2409132c1eb
rahulRathod1234 <rahulrathod34615@gmail.com>
2024-03-05 23:11:33 +0530

    sidebar added

--- src/main/java/com/SmartContactManager/ContactManager/controller/UserController.java
+++ src/main/java/com/SmartContactManager/ContactManager/controller/UserController.java
@@ -1,15 +1,30 @@
 package com.SmartContactManager.ContactManager.controller;
 
 
+import com.SmartContactManager.ContactManager.Entities.User;
+import com.SmartContactManager.ContactManager.smart.dao.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/user")
 public class UserController
 {
+   // @Autowired
+    //private UserRepository userRepository;
     @RequestMapping("/index")
-    public String dashboard(){
+    public String dashboard(Model model, Principal principal)
+    {
+        String userName = principal.getName();
+        System.out.println("USERNAME"+userName);
+
+   // User user = userRepository.getUserByUserName(userName);
+    //    System.out.println("USER:"+user);
+
         return "user_dashboard";
     }
 
--- src/main/java/com/SmartContactManager/ContactManager/smartConfiguration/MyConfig.java
+++ src/main/java/com/SmartContactManager/ContactManager/smartConfiguration/MyConfig.java
@@ -1,10 +1,6 @@
 package com.SmartContactManager.ContactManager.smartConfiguration;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
 public class MyConfig {
 
 }
